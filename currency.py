c57463a79f9d4c90e51960cbc10b183463ccfc71
Dassa <hadassah.harland@gmail.com>
2015-10-11 21:23:03 +1000

    finished but testing on trip still to go

--- currency.py
+++ currency.py
@@ -1,10 +1,9 @@
-__author__ = 'Dassa'
-
-
 import web_utility
-import urllib
-from urllib.request import urlopen
-from urllib.error import URLError
+
+__author__ = 'Dassa'
+# manipulating program to run main() function
+# __name__ = '__main__'
+
 
 def convert(amount, home_currency_code, location_currency_code):
     # url specific for any provided amount, home and location currency codes.
@@ -12,7 +11,6 @@ def convert(amount, home_currency_code, location_currency_code):
         float(amount)
         url = "https://www.google.com/finance/converter?a=" + str(amount) + "&from=" + str(home_currency_code) + "&to=" + str(location_currency_code) + "&meta=ei%3Db-INVuGzDsy60gSThqFg"
         # returns the information provided by the given url
-        print(url)
         result = web_utility.load_page(url)
         # initial shortening of result
         strip_result = (result[result.index('result'):])
@@ -20,27 +18,12 @@ def convert(amount, home_currency_code, location_currency_code):
         split_parts = parts[2].split(" ")
         result = float(split_parts[0])
     except:
+        # General except clause to catch all error types
         result = -1
     return result
 
-# Convert function Trial
-# set parameters
-amount = "erty"      # returns as if = 1 when a string of letters is inserted
-home_currency_code = "USD"
-location_currency_code = "AUD"
-# call function
-print(convert(amount, home_currency_code, location_currency_code))
-# print function
-
-# # input parameters
-# return_amount = convert(input("amount: "), input("home_currency_code: "), input("location_currency_code: "))
-# # print function
-# print(return_amount)
-
 
 def get_details(country_name):
-    i = 0       # position variable
-    country_details_list = None
     # open currency_details.txt file in read only mode
     currency_details = open("currency_details.txt", mode="r", encoding="utf-8")
 
@@ -54,15 +37,30 @@ def get_details(country_name):
     return ()
 
 
-# # Get Details function trial
-# # Set parameters
-# country_name = "United States"      # use title() method
-# # Call function
-# country_details_ea = get_details(country_name)
-# # Print function
-# print(country_details_ea)
-#
-# # input parameters
-# country_details_ea = get_details(input("country_name: "))
-# # Print function
-# print(country_details)
+# Testing
+def main():
+    print("Check convert")
+    print("invalid Conversion           " + str(1.00) + "       AUD -> AUD   " + str(convert(1.00, "AUD", "AUD")))
+    print("invalid Conversion           " + str(1.00) + "       JPY -> ABC   " + str(convert(1.00, "JPY", "ABC")))
+    print("invalid Conversion           " + str(1.00) + "       ABC -> USD   " + str(convert(1.00, "ABC", "USD")))
+    print("Valid Conversion             " + str(10.95) + "      AUD -> JPY   " + str(convert(10.95, "JPY", "AUD")))
+    print("Valid Reversed Conversion    " + str(943.18) + "     JPY -> AUD   " + str(convert(943.18, "JPY", "AUD")))
+    print("Valid Conversion             " + str(10.95) + "      AUD -> BGN   " + str(convert(10.95, "AUD", "BGN")))
+    print("Valid Reversed Conversion    " + str(13.62) + "      BGN -> AUD   " + str(convert(13.62, "BGN", "AUD")))
+    print("Valid Conversion             " + str(200.15) + "     BGN -> JPY   " + str(convert(200.15, "BGN", "JPY")))
+    print("Valid Reversed Conversion    " + str(13589.49) + "   JPY -> BGN   " + str(convert(13589.49, "JPY", "BGN")))
+    print("Valid Conversion             " + str(100.00) + "      JPY -> USD   " + str(convert(100.00, "JPY", "USD")))
+    print("Valid Reversed Conversion    " + str(0.83) + "       USD -> JPY   " + str(convert(0.83, "USD", "JPY")))
+    print("Valid Conversion             " + str(19.99) + "      USD -> BGN   " + str(convert(19.99, "USD", "BGN")))
+    print("Valid Reversed Conversion    " + str(34.58) + "      BGN -> USD   " + str(convert(34.58, "BGN", "USD")))
+    print("Valid Conversion             " + str(19.99) + "      USD -> AUD   " + str(convert(19.99, "USD", "AUD")))
+    print("Valid Reversed Conversion    " + str(27.80) + "       AUD -> USD   " + str(convert(27.80, "USD", "AUD")))
+    print("\nCheck get_details")
+    print("invalid details              " + "Unknown     " + str(get_details("Unknown")))
+    print("invalid details              " + "Japanese    " + str(get_details("Japanese")))
+    print("invalid details              " + "            " + str(get_details("")))
+    print("Valid details                " + "Australia   " + str(get_details("Australia")))
+    print("Valid details                " + "Japan       " + str(get_details("Japan")))
+    print("Valid details                " + "Hong Kong   " + str(get_details("Hong Kong")))
+if __name__ == "__main__":
+    main()
--- trip.py
+++ trip.py
@@ -1,13 +1,15 @@
+import currency
 __author__ = 'Dassa'
 
-    # Attributes: responsible for knowing. Instance Variables
-    # Methods: responsible for doing.
+# Attributes: responsible for knowing. Instance Variables
+# Methods: responsible for doing.
 
 
 class Error(Exception):
     # Use this class to raise exceptions when code generates a runtime error
     def __init__(self, value):
         super().__init__(value)
+
 
 class Country:
     # Use this class to represent the details about a single country
@@ -17,26 +19,40 @@ class Country:
         self.currency_code = currency_code
         self.currency_symbol = currency_symbol
 
-    def format_currency(self, amount):
-        return self.currency_symbol + str(format(float(amount), '.2f'))
-
     def __str__(self):
-        return "Country Details: " + "{} {} {}".format(str(self.country_name) + str(self.currency_code) + str(self.currency_symbol))
+        return "{} {} {}".format(str(self.country_name), str(self.currency_code), str(self.currency_symbol))
+
+    def format_currency(self, amount):
+        # Format amount to be presented as %X.XX where % is the
+        return self.currency_symbol + str(format(float(amount), '.2f'))
 
 
 class Details:
+    # Use this class to determine dates and locations relevant to the trip
     def __init__(self):
         self.locations = []
 
     def add(self, country_name, start_date, end_date):
-        details = (country_name, start_date, end_date)
-        if details in self.locations:
+        trip_details = (country_name, start_date, end_date)
+        # check for invalid dates
+        dates = [start_date.split("/"), end_date.split("/")]
+        for item in dates:
+            # check every character in the date, without the separators, is a digit
+            for part in item:
+                for char in part:
+                    if not char.isdigit():
+                        raise Error("Invalid date")
+            # check the length of the year is 4 digits, and the date and months are 2 digits
+            if len(item[0]) != 4 or len(item[1]) != 2 or len(item[2]) != 2:
+                raise Error("Invalid date")
+        if trip_details in self.locations:
             raise Error("Duplicate Trip")
         if start_date > end_date:
-            raise Error("Date Error")
-        self.locations.append(details)
+            raise Error("Invalid date")
+        self.locations.append(trip_details)
 
-    def current_country(self, date_string): #YYYY/MM/DD
+    def current_country(self, date_string):
+        # Date format YYYY/MM/DD
         current_country = None
         for item in self.locations:
             # if date the date lies between two given dates, then your location is that corresponding location
@@ -47,34 +63,19 @@ class Details:
         else:
             return current_country
 
+    # method is_empty() determines whether locations is empty or not, and returns true if empty
+    def is_empty(self):
+        return self.locations == []
 
-
-
-
-
-    # method is_empty() determines whether locations is empty or not
-    def is_empty(self, locations):
-        if locations == []:
-            return True
-        else:
-            return False
-
-details = Details()
-
-aus_trip = ("Australia", "1997/11/08", "2015/10/11")
-details.add("Australia", "1997/11/08", "2015/10/11")
-print(details)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Testing
+def main():
+    print("Check Country Class")
+    print("Format: Australia, AUD, $, amount = 100  " + Country("Australia", "AUD", "$").format_currency(100))
+    print("\nCheck Details Class")
+    details = Details()
+    details.add("Australia", "2000/01/01", "2015/01/01")
+    print(details.locations)
+# TODO further error checking
+# use a try/except for the invalid things that raise errors...
+if __name__ == "__main__":
+    main()
